871998c3884ad208a9bc9f2c60509df70fccec94
filtelescu <TheCyberPuma@protonmail.com>
2022-03-02 21:43:16 +0200

    complete the first 3 challanges

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    let splitString = string.split("")
+    splitString = splitString.reverse()
+    splitString = splitString.join("")
+
+    return splitString
 
 };
+
+reverseString("hello")
 
 // Do not edit below this line
 module.exports = reverseString;
