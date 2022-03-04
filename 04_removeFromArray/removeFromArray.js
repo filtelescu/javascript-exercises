643a1a001a411ad7a99c06476eb14abacb177bf8
filtelescu <TheCyberPuma@protonmail.com>
2022-03-04 21:40:17 +0200

    solve problem 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
 
+
+const removeFromArray = function(anArray,toRemove) {
+    let removed = Array.prototype.slice.call(arguments, 1)
+
+    for (item in removed) {
+        let testNum = removed[item]
+        index = anArray.indexOf(testNum)
+
+        if (index in anArray) {
+            anArray.splice(index,1)
+        }
+    }
+
+    return anArray
+    // anArray.splice(index,1)
+    
 };
+
+console.log(removeFromArray([1,4,3], 2,3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
