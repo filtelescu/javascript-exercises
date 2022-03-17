1a337b9d17c4ffa4331d1e19a143fdf109f057fd
filtelescu <TheCyberPuma@protonmail.com>
2022-03-17 22:14:29 +0200

    problem 7 solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function(degrees) {
+  let celcius = 0;
+  celcius = (degrees-32)*(5/9)
+  celcius = celcius.toFixed(1)
+  toInt = parseFloat(celcius)
+  return toInt
 };
 
-const ctof = function() {
-
+const ctof = function(degrees) {
+  let fern = 0;
+  fern = (degrees * (9/5)) + 32
+  fern = fern.toFixed(1)
+  toInt = parseFloat(fern)
+  return toInt
 };
+
+console.log(ftoc(100))
 
 // Do not edit below this line
 module.exports = {
